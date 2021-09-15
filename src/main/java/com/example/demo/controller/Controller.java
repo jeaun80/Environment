9f81cd655dd107618fc1e84cb08fae0aee76a3b5
ijin <2zun@naver.com>
2021-09-15 09:53:45 +0900

    login logic 수정
    login logic 수정

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,18 +1,24 @@
 package com.example.demo.controller;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 @org.springframework.stereotype.Controller
-@Log4j2
-@RequiredArgsConstructor
-
 public class Controller {
     @GetMapping("/index")
-    public String index(){
-        return "/index";
-    }
-    @GetMapping("/login")
-    public String login(){ return "/login";}
+    public String index(){ return "/index"; }
+    @RequestMapping(value="/login")
+    public String login() { return "/login"; }
+    @GetMapping("/next")
+    public String next(){ return "/next";}
+    
 }
